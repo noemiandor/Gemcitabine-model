ba5f623812a7ca89690eb4c83e3248cf8bdd91e3
noemiandor <noemi.andor@gmail.com>
2024-04-12 17:42:15 -0400

    tracking based ploidy quantification; PKPD data analysis and fitting

---
+++ Code/Gemcitabine_TrackingResultsAnalysis_V2.R
@@ -0,0 +1,81 @@
+library(matlab)
+library( celltrackR )
+library( fitdistrplus )
+library( xlsx )
+setwd("~/Projects/PMO/HighPloidy_DoubleEdgedSword/data/BreastCancerCLs/SUM159/K00_GemcitabineExposure_033023/")
+devtools::source_url("https://github.com/noemiandor/Utils/blob/master/grpstats.R?raw=TRUE")
+
+assignWGDstatus <- function(track, distr){
+  track = track[order(track$t),]
+  track$WGD=0
+  track_orig=track;
+  i=1
+  WGD=0;
+  while(i<=nrow(track)){
+    # resetting cell cycle clock to zero (as if cell had just divided):
+    track = track[i:nrow(track),,drop=F]
+    track$AreFC=track$Object_Area_0/track$Object_Area_0[1]
+    track_orig[rownames(track),"WGD"]=WGD
+    
+    pdiv=pnorm(track$AreFC,mean = distr$estimate["mean"], sd = distr$estimate["sd"])
+    if(all(pdiv<0.5)){
+      break
+    }
+    # Probability of dividing in the subsequent timestep exceeds 0.5:
+    i=which(pdiv>=0.5)[1]+1
+    WGD=WGD+1;
+  }
+  plot(track_orig$t, track_orig$Object_Area_0, col=track_orig$WGD+1)
+  return(track_orig)
+}
+
+
+
+plateMap=read.xlsx("Gemcitabine_PlateMap_20240111.xlsx", sheetIndex = 1)
+
+# f=list.files("J01_20240111_CellTracking_Ilastik/F_row/F6_1_inter-division/", full.names = T)
+f=list.files("J01_20240111_CellTracking_Ilastik/A_row/A_row_inter-division/", full.names = T)
+daughterParentCells=sapply(f, function(x) read.table(x), simplify = F)
+dt_hours=sapply(daughterParentCells, function(x) quantile(x$t,c(0,1)))
+dt_hours= 2*(dt_hours[2,]-dt_hours[1,])
+hist(dt_hours)
+
+##  inter-division tracks which lasted at least 18 hours (doubling time for SUM-159 is 22 hours).
+daughterParentCells = daughterParentCells[dt_hours>=18];
+pearson_R=sapply(daughterParentCells, function(x) cor(x$t,x$Object_Area_0))
+hist(pearson_R)
+
+## a strong correlation between time and cell area (Pearson r>=0.1), suggesting these are indeed cells that progress through the cell cycle.
+daughterParentCells = daughterParentCells[pearson_R>=0.1];
+
+sizeFoldChange=sapply(daughterParentCells, function(x) x$Object_Area_0[which.max(x$t)]/x$Object_Area_0[which.min(x$t)])
+hist((sizeFoldChange))
+hist(log(sizeFoldChange))
+## @TODO use untreated cells instead of 75% quantile cutoff
+sizeFoldChange=sizeFoldChange[sizeFoldChange<quantile(sizeFoldChange,0.75)]
+d=fitdist(sizeFoldChange,"norm")
+plot(d)
+
+# @TODO: ask Jordan to send all post-division tracks (not just inter-division tracks) and read them in here
+WGD=sapply(daughterParentCells, function(x) assignWGDstatus(x, d), simplify = F)
+
+## WGD distribution per timepoint
+cells=list()
+wgdMax=max(sapply(WGD, function(x) max(x$WGD)))
+tp=1:40
+for(t in tp){
+  x=sapply(WGD, function(x) x[x$t==t,], simplify = F)
+  x=do.call(rbind,x)
+  x=plyr::count(c(x$WGD,0:wgdMax))
+  rownames(x) = x$x
+  x=x[as.character(0:wgdMax),-1,drop=F]
+  cells[[t]] = x
+}
+cells=do.call(cbind,cells)
+colnames(cells)=as.character(tp)
+
+## plot
+barplot(as.matrix(cells), col=rainbow(nrow(cells)), xlab="timepoint", ylab="cell count");
+cells_=sweep(cells,MARGIN = 2, STATS=apply(cells,2,sum), FUN = "/")
+barplot(as.matrix(cells_), col=rainbow(nrow(cells)), xlab="timepoint", ylab="cell fraction");
+legend("bottomleft", as.character(0:wgdMax), fill=rainbow(nrow(cells)),title="WGD",bty = "")
