40e4840c0cdebeeeb2e7630b52a5a9e325be956c
k4kyei <k4kyei@gmail.com>
2024-05-05 22:59:53 -0400

    update 5_5_24

--- Code/Gemcitabine_TrackingResultsAnalysis_V2.R
+++ Code/Gemcitabine_TrackingResultsAnalysis_V2.R
@@ -3,11 +3,16 @@ library(mnormt)
 library(matlab)
 library( celltrackR )
 library( xlsx )
-setwd("/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/A_row/A_row_inter-division/")
+library(ggplot2)
+
+
+maindir="/Users/4477116/Documents/projects/polyploidization/Gemcitabine_model/"
+
+setwd(paste0(maindir,"Data/A_row/A_row_inter-division/"))
 devtools::source_url("https://github.com/noemiandor/Utils/blob/master/grpstats.R?raw=TRUE")
 
 
-OUTD="/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/matlab"
+OUTD=paste0(maindir,"Data/matlab")
 
 assignWGDstatus <- function(track, distr){
   track = track[order(track$t),]
@@ -27,35 +32,39 @@ assignWGDstatus <- function(track, distr){
     dat=t(sapply(timepoints2include:nrow(track), function(x) track$AreFC[(x-(timepoints2include-1)):x] ))
     pdiv=pmnorm(dat, mean = as.numeric(distr$parameters$mean), varcov = distr$parameters$variance$Sigma)
     
-    if(all(!is.na(pdiv)<0.5)){
+    if(is.na(all(pdiv)) | all(pdiv)<0.5){
       break
     }
     # Probability of dividing in the subsequent timestep exceeds 0.5:
-    i=which(!is.na(pdiv)>=0.5)[1]+1
+    i=which(pdiv>=0.5)[1]+1
     WGD=WGD+1;
   }
   plot(track_orig$t, track_orig$Object_Area_0, col=track_orig$WGD+1)
   return(track_orig)
 }
 
-plateMap=read.xlsx("/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/Gemcitabine_PlateMap_20240111.xlsx", sheetIndex = 1)
+plateMap=read.xlsx(paste0(maindir,"Data/Gemcitabine_PlateMap_20240111.xlsx"), sheetIndex = 1)
 
+
+Rows<-c("A","B","E","F")
 train_test<-list(train=c("2"),test=c("3","4","5","6","7","8","9","10","11"))
 
-for(train_row in c("A","F")){
+#train<-apply(expand.grid(Rows, Cols$train), 1, function(x) paste(x, collapse = ""))
+#test<-apply(expand.grid(Rows, Cols$test), 1, function(x) paste(x, collapse = ""))
+
+
+
+for(train_row in Rows){
     tp=1:40; ## timepoints of interest to be recorded for matlab model fit
     timepoints2include=3 ; ## for multivariate gaussian fit
   
-  
-    f=list.files(paste0("/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/",train_row,"_row/",train_row,"_row_inter-division/"),
-                 pattern=paste0("*",train_row,"2.*","\\.txt"), full.names = F)
+    setwd(paste0(maindir,"Data/",train_row,"_row/",train_row,"_row_inter-division/"))
+    f=list.files(".",pattern=paste0("*",train_row,"2.*","\\.txt"), full.names = F)
    
-   setwd(paste0("/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/",train_row,"_row/",train_row,"_row_inter-division/"))
+   
     daughterParentCells=sapply(f, function(x) read.table(x), simplify = F)
     
   
-    
-    
     dt_hours=sapply(daughterParentCells, function(x) quantile(x$t,c(0,1)))
     dt_hours= 2*(dt_hours[2,]-dt_hours[1,])
     hist(dt_hours)
@@ -93,17 +102,16 @@ for(train_row in c("A","F")){
     #sizeFoldChange=sizeFoldChange[,apply(is.finite(sizeFoldChange),2,all)]
     d=mvn("XXX",t(sizeFoldChange)); 
     
-    for(test_row in c("F")){
-      for(col in train_test$test){
-      well=paste0(test_row,col)
+    #@TODO training row is the same as  testing rows, modify the following to allow otherwise
+    test_row=train_row #
+    for(test_col in train_test$test){
+      well=paste0(test_row,test_col)
       # @TODO: use post-division tracks (not just inter-division tracks) and read them in here
-      setwd(paste0("/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/",test_row,"_row/",test_row,"_row_post-division/"))
-      post_divsion=list.files(path = paste0("/Users/john/Documents/IMO/polyploidization/Gemcitabine_model/Data/",test_row,"_row/",test_row,"_row_post-division/"),
-                              pattern=paste0("*",well,".*","\\.txt"), full.names = F)
+      setwd(paste0(maindir,"Data/",test_row,"_row/",test_row,"_row_post-division/"))
+      post_divsion=list.files(".",pattern=paste0("*",well,".*","\\.txt"), full.names = F)
       
           # confirm there is data for this row and column
         daughterParentCells=sapply(post_divsion, function(x)read.table(x),simplify=F)
-      
       
       
       
@@ -130,8 +138,8 @@ for(train_row in c("A","F")){
       rownames(cells)[1]="Dead"
       
       ## Save output for Matlab code
-      train_applied=paste0(test_row,"_",well)
-      write.table(cells,paste0(OUTD,filesep,train_applied,".txt"),row.names = T,quote = F)
+      train_applied=paste0(train_row,"_",well)
+      write.table(cells,paste0(OUTD,filesep,train_applied,".txt"),row.names = TRUE,quote = F)
       
       ## plot: @TODO save plot
       barplot(as.matrix(cells), col=rainbow(nrow(cells)), xlab="timepoint", ylab="cell count");
@@ -141,22 +149,25 @@ for(train_row in c("A","F")){
       
       
       
-      png(file=paste0("/Users/john/Documents/IMO/polyploidization/inter_results/trained_",train_row,"2_ID_applied_",well,"_PD_count.png"),
+      png(file=paste0(maindir,"inter_results/trained_",train_row,"2_ID_applied_",well,"_PD_count.png"),
           width=600, height=538)
       barplot(as.matrix(cells), col=rainbow(nrow(cells)), xlab="timepoint", ylab="cell count");
       dev.off()
       
       cells_=sweep(cells,MARGIN = 2, STATS=apply(cells,2,sum), FUN = "/")
-      png(file=paste0("/Users/john/Documents/IMO/polyploidization/inter_results/trained_",train_row,"2_ID_applied_",well,"_PD_fraction.png"),
+      png(file=paste0(maindir,"inter_results/trained_",train_row,"2_ID_applied_",well,"_PD_fraction.png"),
           width=600, height=538)
       barplot(as.matrix(cells_), col=rainbow(nrow(cells)), xlab="timepoint", ylab="cell fraction");
       legend("bottomleft", as.character(0:wgdMax), fill=rainbow(nrow(cells)),title="WGD",bty = "")
       dev.off()
+      
+      
+      
         }
        }
       }
    }
-}
+
 
 
 
