b76ee7ec98eae92b574259511edea8a818f4020b
k4kyei <k4kyei@gmail.com>
2024-05-27 13:58:11 -0400

    update 5_27_24

--- Code/cost.m
+++ Code/cost.m
@@ -1,20 +1,25 @@
-function J  = cost(pars)
-global dmx;
+function J  = cost(pars,dmx,DOSE)
+
 
 %% Parameters
+% u=cell2mat(pars(1));
+% w1=cell2mat(pars(2));
+% v=cell2mat(pars(3));
+% nu = cell2mat(pars(4));
+
+
 u=pars(1);
 w1=pars(2);
 v=pars(3);
-nu = pars(4);
-
+nu=pars(4);
 %% @TODO: initial point should be a parameter
 
 %% Define the initial conditions
 tspan=(2:2:size(dmx.N2,2)*2);
 
 %% Solve
-[t_2N,y_2N] = ode45(@(t,y) skippedMito_ODE(t,y,u,v,w1, 0, nu), tspan, dmx.N2(:,1));
-[t_4N,y_4N] = ode45(@(t,y) skippedMito_ODE(t,y,u,v,w1, 1, nu), tspan, dmx.N4(:,1));
+[t_2N,y_2N] = ode45(@(t,y) skippedMito_ODE(t,y,u,v,w1, 0, nu,DOSE), tspan, dmx.N2(:,1));
+[t_4N,y_4N] = ode45(@(t,y) skippedMito_ODE(t,y,u,v,w1, 1, nu,DOSE), tspan, dmx.N4(:,1));
 fits=struct('N2',{t_2N,y_2N}, 'N4',{t_4N,y_4N});
 
 %% Bring simulations and measurements to same dimensions
@@ -35,28 +40,28 @@ for type=fieldnames(dmx)'
     end
     wsdAll=[wsdAll,mean(wsd)];
 
-%     %% plot
-%     mycolors = flip(colormap(parula(size(dmx__,1))));
-%     subplot(1,2,idx); idx=idx+1;
-%     hold off;
-%     for cI = 1:size(dmx__,1)
-%         plot(t_, y_(cI,:),'Color',mycolors(cI,:),'LineWidth',3)
-% %         set(gca, 'XScale', 'log')
-% %         set(gca, 'YScale', 'log')
-% %         ylim([1,1000])
-%         hold on;
-%         plot(t_, dmx__(cI,:),'*--','Color',mycolors(cI,:),'LineWidth',3)
-% %         ylim([0,65])
-% %         title(type{1})
-%     end
-%     xlabel('Time (hours)')
-%     ylabel('Number of cells')
-% %     legend('D','$$\hat{D}$$','Interpreter','Latex')
-%     prefix='D';
-%     if strcmp(type{1},'N4')
-%         prefix='T';
-%     end
-%     legend('A','$$\hat{A}$$',['$$',prefix,'_0$$'],['$$\hat{',prefix,'_0}$$'],['$$',prefix,'_1$$'],['$$\hat{',prefix,'_1}$$'],['$$',prefix,'_2$$'],['$$\hat{',prefix,'_2}$$'],'Interpreter','Latex')
+    %% plot
+    mycolors = flip(colormap(parula(size(dmx__,1))));
+    subplot(1,2,idx); idx=idx+1;
+    hold off;
+    for cI = 1:size(dmx__,1)
+        plot(t_, y_(cI,:),'Color',mycolors(cI,:),'LineWidth',3)
+        %set(gca, 'XScale', 'log')
+        %set(gca, 'YScale', 'log')
+        ylim([1,1000])
+        hold on;
+        plot(t_, dmx__(cI,:),'*--','Color',mycolors(cI,:),'LineWidth',3)
+        ylim([0,65])
+        title(type{1})
+    end
+    xlabel('Time (hours)')
+    ylabel('Number of cells')
+%     legend('D','$$\hat{D}$$','Interpreter','Latex')
+    prefix='D';
+    if strcmp(type{1},'N4')
+        prefix='T';
+    end
+    legend('A','$$\hat{A}$$',['$$',prefix,'_0$$'],['$$\hat{',prefix,'_0}$$'],['$$',prefix,'_1$$'],['$$\hat{',prefix,'_1}$$'],['$$',prefix,'_2$$'],['$$\hat{',prefix,'_2}$$'],'Interpreter','Latex')
 
 end
 
