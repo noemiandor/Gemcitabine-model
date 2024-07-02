8d9284bf87d41e43ba656b5c0be33d97da658004
noemiandor <noemi.andor@gmail.com>
2024-07-01 22:29:45 -0400

    model dFdCTP incorporated into cells DNA

--- Code/Manager_combined.m
+++ Code/Manager_combined.m
@@ -6,17 +6,15 @@ cd(path2root)
 addpath(path2root)
 
 global dmx
-global GemcitabineConc_nM
-GemcitabineConc_nM=1000;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Fit model to data %%%
 S = struct();
-% @TODO: we need to read in the plate map here 
-% @TODO: we need to somehow normalize the drug concentrations in the PKPD data to the cell concentration in the PKPD experiment (which is different from the cell concentration in the Incucyte experiment)
+% @TODO: we need to read in the plate map here
 % @TODO: we need to use wells that have the same (or similar) normalized drug concentration as the starting concentration in the PKPD experiment
 replicates = struct('N2', {'B_4', 'B_5', 'B_6'}, ...
-                    'N4', {'E_4', 'E_5', 'E_6'});
+    'N4', {'E_4', 'E_5', 'E_6'});
+dfdctpConc_incucyte_nM=15; %@TODO: we need to figure out to which intra-cellular dFdCTP concentration each well corresponds at timepoint zero 
 
 %% Iterate across replicates
 for k = 1:length(replicates.N2)
@@ -27,29 +25,27 @@ for k = 1:length(replicates.N2)
     pkpd_file = dir(['nM1000_2N-', num2str(k), '.txt']);
     pkpd_data = readtable(pkpd_file.name);
     dmx.N2dFdCTP= pkpd_data.dFdCTP___ng_mL_';
-    pkpd_file = dir(['nM1000_4N-', num2str(k), '.txt']);
-    pkpd_data = readtable(pkpd_file.name);
-    dmx.N4dFdCTP = pkpd_data.dFdCTP___ng_mL_';
     dmx.PKPDtime = pkpd_data.time';
 
-    % Read cell data for N2 and N4
+    % Read cell data for N2
     cd(path2celldata)
     dmx.N2 = readtable([replicates(k).N2, '.txt']);
     dmx.N2 = table2array(dmx.N2(:, 2:end)); % assuming data starts from the second column
-    dmx.N4 = readtable([replicates(k).N4, '.txt']);
-    dmx.N4 = table2array(dmx.N4(:, 2:end)); % assuming data starts from the second column
-    dmx.cellTime = 2:2:size(dmx.N4, 2) * 2;
+    dmx.cellTime = 2:2:size(dmx.N2, 2) * 2;
+    % plot(dmx.cellTime, dmx.N2(2,:)); xlabel('hour'); ylabel('# diploid cells')
     cd(path2root)
+
+    %% correct data
+    ii = find(dmx.cellTime<55);
+    N2_interpolated = interp1(dmx.cellTime(ii), dmx.N2(2,ii), dmx.cellTime, 'linear', 'extrap');
+    dmx.N2(2,:)=N2_interpolated;
 
     % Interpolate PKPD data to match cellTime points
     N2_interpolated = interp1(dmx.PKPDtime, dmx.N2dFdCTP, dmx.cellTime, 'linear', 'extrap');
-    N4_interpolated = interp1(dmx.PKPDtime, dmx.N4dFdCTP, dmx.cellTime, 'linear', 'extrap');
     N2_interpolated(N2_interpolated<0)=0;
-    N4_interpolated(N4_interpolated<0)=0;
-
+   
     % Visualize the interpolation
     figure;
-    subplot(1, 2, 1);
     plot(dmx.PKPDtime, dmx.N2dFdCTP, 'o-', 'DisplayName', 'Original High');
     hold on;
     plot(dmx.cellTime, N2_interpolated, '*-', 'DisplayName', 'Interpolated High');
@@ -58,48 +54,52 @@ for k = 1:length(replicates.N2)
     ylabel('Concentration (ng/mL)');
     legend;
 
-    subplot(1, 2, 2);
-    plot(dmx.PKPDtime, dmx.N4dFdCTP, 'o-', 'DisplayName', 'Original Low');
-    hold on;
-    plot(dmx.cellTime, N4_interpolated, '*-', 'DisplayName', 'Interpolated Low');
-    title('Low Concentration Interpolation');
-    xlabel('Time');
-    ylabel('Concentration (ng/mL)');
-    legend;
-    
-    % Update dmx with interpolated values
+    % Match pkpd drug concentration to incucyte drug concentration
+    [~,ia]=min(abs(dfdctpConc_incucyte_nM-N2_interpolated));
+    N2_interpolated = interp1(dmx.cellTime(1:(1+end-ia)), N2_interpolated(ia:end), dmx.cellTime, 'linear', 'extrap');
     dmx.N2dFdCTP = N2_interpolated;
-    dmx.N4dFdCTP = N4_interpolated;
 
-    %% Do the fitting
+    %% Do the fitting for one cell type at a time
+    ii = 1:length(dmx.cellTime); %find(dmx.cellTime<=55);
+    dmx.dFdCTP = dmx.N2dFdCTP(ii);
+    dmx.cells = dmx.N2(:,ii);
+    % need to add zero for cells of highest ploidy + 1 orelse the model will not penalize their existence
+    dmx.cells = [dmx.cells; zeros(1, size(dmx.cells,2))];
+    dmx.cellTime = dmx.cellTime(ii);
+   
     A = [];
     b = [];
     Aeq = [];
     beq = [];
 
     % Combined parameters for both models
-    theta = 50; nu_gemcitabine = 1500; eta = 0.005; xi = 0.05;
-    a = 150; v = 65; w1 = 310; iota = 0;
-    pars = {theta, nu_gemcitabine, eta, xi, a, v, w1, iota};
-    bounds = cell2mat(cellfun(@(x) [x/5000; x*5000], pars, 'UniformOutput', false));
+    theta = 5e-1; nu = 4.0378; 
+    eta =  0.3340; xi = 7.3125; a = 1.0000; 
+    v = 381.4379; w1 = 20; iota = 0;
+    gamma_a = 0.1000; lambda = 1.0000;
+    pars = {theta, nu, eta, xi, a, v, w1, iota, gamma_a, lambda };
+    bounds = cell2mat(cellfun(@(x) [x/10; x*10], pars, 'UniformOutput', false));
+    % bounds(:,2) = [pars{2}/10; pars{2}*10];
+    % bounds(:,6) = [pars{6}/10; pars{6}*10];
     lb = bounds(1,:)';
     ub = bounds(2,:)';
 
     opts = optimoptions(@fmincon);
 
     problem = createOptimProblem('fmincon', 'objective', ...
-        @(pars) combined_cost(pars, dmx, GemcitabineConc_nM), 'x0', cell2mat(pars), 'lb', lb, 'ub', ub, 'options', opts);
+        @(pars) combined_cost(pars, dmx), 'x0', cell2mat(pars), 'lb', lb, 'ub', ub, 'options', opts);
 
-    rs = RandomStartPointSet('NumStartPoints', 2);
+    rs = RandomStartPointSet('NumStartPoints', 5);
     points = list(rs, problem);
     ms = MultiStart('UseParallel', true);
     [pars_, fval, exitflag, output, solutions] = run(ms, problem, CustomStartPointSet(points));
 
-    fname = [replicates(k).N2,'_and_', replicates(k).N4];
-    S = setfield(S, fname, pars_);
+    S = setfield(S, replicates(k).N2, pars_);
+    % save('solutions.mat','pars_');
+    % pars=arrayfun(@(x) {x}, pars_);
 
     %% Plot best fit:
     % close all hidden
-    figure('name', ['~/Downloads/Combined_model_', fname], 'Position', [100 100 1000 400])
-    combined_cost(getfield(S, fname), dmx, GemcitabineConc_nM)
+    figure('name', ['~/Downloads/Combined_model_', replicates(k).N2], 'Position', [100 100 1000 400])
+    combined_cost(getfield(S, replicates(k).N2), dmx)
 end
--- Code/combined_cost.m
+++ Code/combined_cost.m
@@ -1,69 +1,53 @@
-function cost = combined_cost(params, dmx, GemcitabineConc_nM)
+function cost = combined_cost(params, dmx)
 
 % Initial conditions
-initial_conditions_N2 = [GemcitabineConc_nM; dmx.N2dFdCTP(1); dmx.N2(:,1)];
-initial_conditions_N4 = [GemcitabineConc_nM; dmx.N4dFdCTP(1); dmx.N4(:,1)];
+initial_conditions = [ dmx.dFdCTP(1); 0; dmx.cells(:,1)];
 
-% Simulate the combined ODE model for N2 population
-[t_N2, y_N2] = ode45(@(t, y) combined_ODE(t, y, params), dmx.cellTime, initial_conditions_N2);
-model_N2 = y_N2(:, 1);
-model_drugN2 = y_N2(:, 4);
-
-% Simulate the combined ODE model for N4 population
-[t_N4, y_N4] = ode45(@(t, y) combined_ODE(t, y, params), dmx.cellTime, initial_conditions_N4);
-model_N4 = y_N4(:, 1);
-model_drugN4 = y_N4(:, 4);
+% Simulate the combined ODE model for population
+[t_N, y_N] = ode15s(@(t, y) combined_ODE(t, y, params), dmx.cellTime, initial_conditions);
+model_drugN= y_N(:, 1:2);
+model_N  = y_N(:, 3:end);
+if size(model_N,1)<size(dmx.cells,2)
+    cost = inf;
+    return;
+end
 
 % @TODO: we should probably use the wasserstein distance again
 % Calculate the cost as the sum of squared differences between model and data
-cost_N2 = sum(sum((model_N2 - dmx.N2').^2));
-cost_N4 = sum(sum((model_N4 - dmx.N4').^2));
-cost_drugN2 = sum((model_drugN2- dmx.N2dFdCTP').^2);
-cost_drugN4 = sum((model_drugN4 - dmx.N4dFdCTP').^2);
+cost_N = sum(sum((model_N(:,1:end) - dmx.cells(1:end,:)').^2)) / max(max((dmx.cells)))^2;
+cost_drugN = sum((model_drugN(:,1)- dmx.dFdCTP').^2) / max(dmx.dFdCTP)^2;
 
-%% plotting 
-fits=struct('N2',{t_N2,y_N2}, 'N4',{t_N4,y_N4});
-idx=1;
-typecolors=struct('N2','black','N4','red');
-for type= {'N2', 'N4'}
-    t=getfield(fits,{1},type{1});
-    y=getfield(fits,{2},type{1});
-    dmx_=getfield(dmx,type{1});
 
-    %% plot
-    mycolors = flip(colormap(parula(size(dmx_,1))));
-    subplot(1,3,idx); idx=idx+1;
-    hold off;
-    for cI = 1:size(dmx_,1)
-        plot(t, y(:,cI),'Color',mycolors(cI,:),'LineWidth',3)
-        %         set(gca, 'XScale', 'log')
-        %         set(gca, 'YScale', 'log')
-        %         ylim([1,1000])
-        hold on;
-        plot(t, dmx_(cI,:),'*--','Color',mycolors(cI,:),'LineWidth',3)
-        %         ylim([0,65])
-        %         title(type{1})
-    end
+%% plot
+mycolors = flip(colormap(parula(size(dmx.cells,1))));
+subplot(1,2,1);
+hold off;
+for cI = 1:size(dmx.cells,1)
+    plot(t_N, model_N(:,cI),'Color',mycolors(cI,:),'LineWidth',3)
+    %         set(gca, 'XScale', 'log')
+    %         set(gca, 'YScale', 'log')
+    %         ylim([1,1000])
+    hold on;
+    plot(t_N, dmx.cells(cI,:),'*--','Color',mycolors(cI,:),'LineWidth',3)
+    %         ylim([0,65])
+    %         title(type{1})
+
     xlabel('Time (hours)')
     ylabel('Number of cells')
     %     legend('D','$$\hat{D}$$','Interpreter','Latex')
     prefix='D';
-    if strcmp(type{1},'N4')
-        prefix='T';
-    end
     legend('A','$$\hat{A}$$',['$$',prefix,'_0$$'],['$$\hat{',prefix,'_0}$$'],['$$',prefix,'_1$$'],['$$\hat{',prefix,'_1}$$'],['$$',prefix,'_2$$'],['$$\hat{',prefix,'_2}$$'],'Interpreter','Latex')
-    %% plot dFdCTP
-    subplot(1,3,3)
-    plot(t, y(:,end),'Color',getfield(typecolors,type{1}),'LineWidth',3)
-    hold on;
-    plot(t, getfield(dmx,[type{1},'dFdCTP']),'*--','Color',getfield(typecolors,type{1}),'LineWidth',3)
-
 end
-legend([fieldnames(typecolors);fieldnames(typecolors)])
+%% plot dFdCTP
+subplot(1,2,2); hold on;
+plot(t_N, dmx.dFdCTP,'*--','Color','black','LineWidth',3)
+plot(t_N, model_drugN(:,1),'Color','black','LineWidth',3)
+yyaxis right
+plot(t_N, model_drugN(:,2),'Color','red','LineWidth',3)
+legend({'dFdCTP__data','dFdCTP__model','dna__dfdctp'})
 
-
-% Combine the costs for N2 and N4
-cost = cost_N2 + cost_N4 + cost_drugN2 + cost_drugN4;
+% Combine the costs for cells and drug
+cost = cost_drugN + cost_N; 
 
 
 end
