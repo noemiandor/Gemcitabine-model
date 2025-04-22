0799dbf25e645001d58c2e3d1590b7f773dd367b
noemiandor <noemi.andor@gmail.com>
2025-04-22 04:44:53 -0400

    chromosome arm level comparison

--- Code/in-vivo/Manager.R
+++ Code/in-vivo/Manager.R
@@ -1,3 +1,4 @@
+library(xlsx)
 library(gplots)
 library(data.table)
 devtools::source_url("https://github.com/noemiandor/Utils/blob/master/grpstats.R?raw=TRUE")
@@ -8,10 +9,17 @@ x <- fread("http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/cytoBand.txt
            col.names = c("chrom","chromStart","chromEnd","name","gieStain"))
 chrarms=x[ , .(length = sum(chromEnd - chromStart)),by = .(chrom, arm = substring(name, 1, 1)) ]
 chrwhole=grpstats(as.matrix(chrarms$length),chrarms$chrom, "sum")$sum
+rownames(chrwhole)=gsub("chrY","chr24",gsub("chrX","chr23",rownames(chrwhole)))
 
 setwd("~/Projects/PMO/HighPloidy_DoubleEdgedSword/data/BreastCancerOrthotopicModels/SUM-159")
 
-
+# weighted Manhattan distance for an *entire* matrix
+chrWeightedDist <- function(mat) {
+  # vector of chromosome weights
+  w <- chrwhole[paste0("chr", 1:22), 1]
+  mat.w <- sweep(mat, 2, w, `*`)         # weight every column
+  dist(mat.w, method = "manhattan") / sum(w)
+}
 
 #############################
 #### scRNA-seq results ######
@@ -26,6 +34,7 @@ col =RColorBrewer::brewer.pal(length(origin),"Paired")
 names(col) = origin
 hm=heatmap.2(cn[[batch]]$cn, Colv = NULL, trace = "n", RowSideColors = col[cn[[batch]]$cells])
 legend("topright",names(col),fill = col,cex=0.5)
+
 
 #############################
 #### Karyotyping results ####
@@ -54,44 +63,57 @@ for(s in names(samples)){
     karyo[id,as.character(fr$x)]=fr$freq
     # ploidy[id]=sum(cell$Chromosome.Length)
     numchrcopies[id]=nrow(cell)
-    ploidy[id]=sum(cell$Chromosome.Area)
+    # ploidy[id]=sum(cell$Chromosome.Area)
+    ploidy[id]=sum(chrwhole[paste0("chr",cell$Group.ID),])/sum(chrwhole)
   }
   heatmap.2(karyo,trace='n',main = s)
   # rownames(karyo)= formatNames(rownames(karyo))
   # names(ploidy)= formatNames(names(ploidy))
   samples[[s]]=list(karyo=karyo,ploidy=ploidy, numchrcopies=numchrcopies)
 }
+ploidy =sapply(samples, function(x) x$ploidy, simplify = F)
 whole_chr_karyo =do.call(rbind,sapply(samples, function(x) x$karyo, simplify = F))
 write.table(whole_chr_karyo, file="~/Downloads/whole_chr_karyo.txt", sep="\t", quote=F)
+par(mai=c(0.5,2,0.5,0.5)); boxplot(ploidy[-c(1:2)],las=2, horizontal = T)
+
+whole_chrarm_karyo = read.xlsx("B02_Karyotyping/SUM159-4N-parental/SUM159_4N_Karyotyping_ArmLevel.xlsx",sheetIndex =1, check.names=F);
+whole_chrarm_karyo=whole_chrarm_karyo[apply(!is.na(whole_chrarm_karyo),1,all),]
 
 
 #######################################
 #### Merge scRNA-seq & Karyotyping ####
 # cn=cn[-2]
 # subset="none"
-subset="2N"
-cn_=cn;
+subset="4N"
 whole_chr_karyo_ = whole_chr_karyo;
 if(subset!="none"){
-  cn_ = sapply(cn, function(x) list(cn=x$cn[grep(subset,x$cells),]), simplify = F)
-  cn_[[1]]$cells = grep(subset,cn[[1]]$cells, value=T)
-  cn_[[2]]$cells = grep(subset,cn[[2]]$cells, value=T)
+  ii=grep(subset,names(cn));
+  cn_ = sapply(cn[ii], function(x) list(cn=x$cn[grep(subset,x$cells),]), simplify = F)
+  cn_[[1]]$cells = grep(subset,cn[[ii]]$cells, value=T)
+  cn_[[1]]$anno = cn[[ii]]$anno
   whole_chr_karyo_=whole_chr_karyo[grep(subset,rownames(whole_chr_karyo_)),]
 }
+## Align segments
+la=alignCNmatrices(whole_chrarm_karyo, cn_[[1]])
+cn_[[1]]$cn = la$cn_scRNAseq
+whole_chr_karyo_ = la$cn_karyo
 
+## select cells of interest
 whole_chr_scRNA=do.call(rbind,sapply(cn_, function(x) x$cn[grep("Cell-Culture",x$cells),], simplify = F))
 if(subset=="4N"){
   ## Numbat output needs to be adjusted for tetraploidy
   whole_chr_scRNA = whole_chr_scRNA * 2
 }
-# pdf(paste0("~/Downloads/",subset,".pdf"))
 
+mar=c(20,5)
+pdf(paste0("~/Downloads/",subset,".pdf"))
+## scRNAseq
 cells=sapply(cn_, function(x) grep("Cell-Culture",x$cells, value=T), simplify = F)
 cells=unlist(cells)
 origin=unique(cells)
 col =rainbow(length(origin)*1.1)[1:length(origin)]
 names(col) = origin
-hm_s=heatmap.2(whole_chr_scRNA, Colv = NULL, trace = "n", RowSideColors =col[cells], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"))
+hm_s=heatmap.2(whole_chr_scRNA, Colv = NULL, trace = "n", RowSideColors =col[cells], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"), margins = mar) 
 legend("topright",names(col),fill=col, cex=0.65)
 cl_s=cutree(as.hclust(hm_s$rowDendrogram), k=8)
 fr=plyr::count(cl_s);
@@ -99,30 +121,32 @@ fr$freq=round(fr$freq/nrow(whole_chr_scRNA),3)
 origin=unique(cl_s)
 col =rainbow(length(origin)*1.1)[1:length(origin)]
 names(col) = as.character(origin)
-heatmap.2(whole_chr_scRNA, Colv = NULL, trace = "n", RowSideColors =col[as.character(cl_s)], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"))
+heatmap.2(whole_chr_scRNA, Colv = NULL, trace = "n", RowSideColors =col[as.character(cl_s)], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"), margins = mar) 
 cl_s = grpstats(whole_chr_scRNA,cl_s,"mean")$mean
 rownames(cl_s) = paste0(rownames(cl_s),"_", fr[rownames(cl_s),"freq"])
 
+## Karyotyping
 cells=sapply(strsplit(rownames(whole_chr_karyo_),"_"),"[[",1)
 origin=unique(cells)
 col =rainbow(length(origin)*1.1)[1:length(origin)]
 names(col) = origin
-hm_k=heatmap.2(whole_chr_karyo_, Colv = NULL, trace = "n", RowSideColors =col[cells], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"))
+hm_k=heatmap.2(whole_chr_karyo_, Colv = NULL, trace = "n", RowSideColors =col[cells],distfun=chrWeightedDist, margins = mar) 
 legend("topright",names(col),fill=col, cex=0.65)
-cl_k=cutree(as.hclust(hm_k$rowDendrogram), k=8)
-heatmap.2(whole_chr_karyo_, Colv = NULL, trace = "n", RowSideColors =col[cells], colRow =  cl_k, hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"))
+cl_k=cutree(as.hclust(hm_k$rowDendrogram), k=2)
+heatmap.2(whole_chr_karyo_, Colv = NULL, trace = "n", RowSideColors =col[cells], colRow =  cl_k,distfun=chrWeightedDist, margins = mar) 
 legend("topright",names(col),fill=col, cex=0.65)
 cl_k = grpstats(whole_chr_karyo_,cl_k,"mean")$mean
+
 
 cn_comb = rbind(cl_k,cl_s)
 # cn_comb = cn_comb[,-5]
 rownames(cn_comb) = paste0(rownames(cn_comb),"_",c(rep("karyo",nrow(cl_k)),rep("sc",nrow(cl_s))))
-hm=heatmap.2(cn_comb, Colv = NULL, trace = "n", hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"))
+hm=heatmap.2(cn_comb, Colv = NULL, trace = "n", hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"), margins = mar) 
 cl=cutree(as.hclust(hm$rowDendrogram), k=8)
 origin=unique(cl)
 col =rainbow(length(origin)*1.1)[1:length(origin)]
 names(col) = as.character(origin)
-heatmap.2(cn_comb, Colv = NULL, trace = "n", RowSideColors =col[as.character(cl)], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"), main=subset)
+heatmap.2(cn_comb, Colv = NULL, trace = "n", RowSideColors =col[as.character(cl)], hclustfun=function(x) hclust(x, method="ward.D2"),distfun=function(x) dist(x, method="manhattan"), main=subset, margins = mar) 
 
 dev.off()
 
--- Code/in-vivo/Utils.R
+++ Code/in-vivo/Utils.R
@@ -165,25 +165,26 @@ NumbatPostProcess <- function(DATASETID="CNV", mpoi=NULL, path2karyo="/Users/448
     # print(p)
     # dev.off()
     
-    ## set copy number of chromosome to copy number of largest segment for that chromosome
-    segments= sapply(sapply(strsplit(colnames(cn),":"),"[[",2), function(x) strsplit(x[[1]],"-")[[1]],simplify = F)
-    segments= as.data.frame(do.call(rbind,sapply(segments, as.numeric,simplify = F)))
-    rownames(segments) = colnames(cn)
-    colnames(segments) = c("start","end")
-    segments$length=1+segments$end-segments$start
-    segments$chr = as.numeric(sapply(strsplit(colnames(cn),":"),"[[",1))
-    chrsegments=sapply(unique(segments$chr), function(x) segments[segments$chr==x,,drop=FALSE],simplify = F)
-    chrsegments=sapply(chrsegments, function(x) x[which.max(x$length),,drop=F],simplify = F)
-    chrsegments = do.call(rbind,chrsegments)
+    anno=parseLOCUS(colnames(cn))
+    anno=addChrAnnotation(anno)
+    anno=as.data.frame(anno)
+    rownames(anno)=paste0(anno$chr,":",anno$startpos,"-",anno$endpos)
     
-    cn=cn[,rownames(chrsegments)]
-    colnames(cn)=chrsegments$chr
-    
-    # gplots::heatmap.2(cn,trace='n',symbreaks = F,symkey=F)
-    
-    
+    # ## set copy number of chromosome to copy number of largest segment for that chromosome
+    # segments= sapply(sapply(strsplit(colnames(cn),":"),"[[",2), function(x) strsplit(x[[1]],"-")[[1]],simplify = F)
+    # segments= as.data.frame(do.call(rbind,sapply(segments, as.numeric,simplify = F)))
+    # rownames(segments) = colnames(cn)
+    # colnames(segments) = c("start","end")
+    # segments$length=1+segments$end-segments$start
+    # segments$chr = as.numeric(sapply(strsplit(colnames(cn),":"),"[[",1))
+    # chrsegments=sapply(unique(segments$chr), function(x) segments[segments$chr==x,,drop=FALSE],simplify = F)
+    # chrsegments=sapply(chrsegments, function(x) x[which.max(x$length),,drop=F],simplify = F)
+    # chrsegments = do.call(rbind,chrsegments)
+    # cn=cn[,rownames(chrsegments)]
+    # colnames(cn)=chrsegments$chr
+
     ## all other chromosomes have copy number equal to ploidy for all cells
-    otherchr = setdiff(1:22,colnames(cn))
+    otherchr = setdiff(rownames(anno),colnames(cn))
     cn_ = matrix(ploidies[patient],nrow(cn),length(otherchr))
     colnames(cn_)=otherchr
     cn = cbind(cn,cn_)
@@ -193,17 +194,383 @@ NumbatPostProcess <- function(DATASETID="CNV", mpoi=NULL, path2karyo="/Users/448
     # dev.off()
     # graphics.off()
     
-    ## karyotype frequency across timepoints
-    cn=round(cn)
-    Chr_seq<-as.character(seq(1,22,1))
-    cn <- cn[, Chr_seq]
-    #cn %>% select(all_of(Chr_seq))
-    
-    outputs[[patient]] = list(cn=cn, cells=sample)
+   
+    outputs[[patient]] = list(cn=cn, cells=sample, anno=anno)
     
     # ## Read expression data:
     # la=readRDS(paste0(path2numbat,patient,"/",patient,".count.rds"))
     
   }
   return(outputs)
+}
+
+
+parseLOCUS <- function(loci) {
+  chr = as.numeric(sapply(strsplit(loci, ":"), "[[", 1))
+  startend = sapply(strsplit(loci, ":"), "[[", 2)
+  startp = as.numeric(sapply(strsplit(startend, "-"), "[[", 1))
+  endp = as.numeric(sapply(strsplit(startend, "-"), "[[", 2))
+  seglength = 1 + endp - startp
+  dm = cbind(chr, startp, endp, seglength) 
+  colnames(dm) = c("chr", "startpos", "endpos", "seglength")
+  return(dm)  
+}           
+
+
+addChrAnnotation <- function(y_mat){
+  ## Expected chromosome lengths ##
+  x <- fread("http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/cytoBand.txt.gz", 
+             col.names = c("chrom","chromStart","chromEnd","name","gieStain"))
+  
+  
+  # --- Start Processing ---
+  
+  # A. Get Chromosome Arm Boundaries from x
+  x[, arm := substr(name, 1, 1)]
+  x[, chr_std := chrom] # Use existing 'chr' column name if already standardized
+  # Handle potential numeric chr names in x if necessary (our example uses 'chr' prefix)
+  # x[grep("^\\d+$", chrom), chr_std := paste0("chr", chrom)]
+  # x[chrom == "X", chr_std := "chrX"]
+  # x[chrom == "Y", chr_std := "chrY"]
+  
+  # Calculate boundaries for p and q arms for all chromosomes in x
+  arm_boundaries <- x[arm %in% c('p', 'q'),
+                      .(p_start = min(fifelse(arm == 'p', chromStart, Inf)), # Min start for p-bands
+                        p_end   = max(fifelse(arm == 'p', chromEnd, -Inf)), # Max end for p-bands
+                        q_start = min(fifelse(arm == 'q', chromStart, Inf)), # Min start for q-bands
+                        q_end   = max(fifelse(arm == 'q', chromEnd, -Inf))  # Max end for q-bands
+                      ), by = .(chr_std)]
+  
+  # Clean up infinite values if an arm is entirely missing in x
+  arm_boundaries[is.infinite(p_start), p_start := NA]
+  arm_boundaries[is.infinite(p_end), p_end := NA]
+  arm_boundaries[is.infinite(q_start), q_start := NA]
+  arm_boundaries[is.infinite(q_end), q_end := NA]
+  
+  # B. Initial Processing of y (Assign Arms)
+  # Prepare x_arms for overlap (only p and q bands)
+  x_arms <- x[arm %in% c('p', 'q')]
+  setkey(x_arms, chr_std, chromStart, chromEnd)
+  
+  # Prepare y
+  y <- as.data.table(y_mat)
+  y[, y_id := .I]
+  if (!any(grepl("^chr", y$chr))) {
+    y[, chr_std_y := paste0("chr", chr)]
+  } else {
+    y[, chr_std_y := as.character(chr)] # Ensure character
+  }
+  y[chr == 23, chr_std_y := "chrX"]
+  y[chr == 24, chr_std_y := "chrY"]
+  setkey(y, chr_std_y, startpos, endpos)
+  
+  # Find overlaps
+  overlaps <- foverlaps(y, x_arms,
+                        by.x = c("chr_std_y", "startpos", "endpos"),
+                        by.y = c("chr_std", "chromStart", "chromEnd"),
+                        type = "any", mult = "all", nomatch = NULL)
+  
+  # Initialize y with 'unknown' arm - will be overwritten if overlaps found
+  y[, arm := "unknown"]
+  
+  if (!is.null(overlaps) && nrow(overlaps) > 0) {
+    # Calculate overlap length
+    overlaps[, overlap_len := pmax(0, pmin(endpos, chromEnd) - pmax(startpos, chromStart))]
+    
+    # Aggregate overlap length by original segment and arm
+    arm_overlaps <- overlaps[, .(total_overlap = sum(overlap_len)), by = .(y_id, arm)] # Use arm from x_arms
+    
+    # Reshape data
+    arm_summary <- dcast(arm_overlaps, y_id ~ arm, value.var = "total_overlap", fill = 0)
+    
+    # Ensure p and q columns exist
+    if (!"p" %in% names(arm_summary)) arm_summary[, p := 0]
+    if (!"q" %in% names(arm_summary)) arm_summary[, q := 0]
+    
+    # Assign Arm Classification
+    arm_summary[, arm_assigned := fifelse(p > 0 & q > 0, "whole",
+                                          fifelse(p > 0, "p",
+                                                  fifelse(q > 0, "q", "unknown")))]
+    
+    # Merge assigned arm back to y
+    # Need to handle potential duplicates in y_id if merge creates them (shouldn't with y_id)
+    y <- merge(y, arm_summary[, .(y_id, arm_assigned)], by = "y_id", all.x = TRUE)
+    
+    # Update the 'arm' column, keeping 'unknown' for those with no p/q overlap found
+    y[!is.na(arm_assigned), arm := arm_assigned]
+    y[, arm_assigned := NULL] # Remove temporary merge column
+    
+  } else {
+    warning("No overlaps found between y segments and p/q arms in x. Initial arms set to 'unknown'.")
+  }
+  
+  
+  # C. Identify and Create Missing Arm Placeholders
+  # Find which chromosome arms are represented in the processed y
+  # Using original numeric 'chr' column for grouping
+  y_arm_presence <- y[, .(has_p = any(arm %in% c('p', 'whole')),
+                          has_q = any(arm %in% c('q', 'whole'))),
+                      by = .(chr, chr_std_y)] # Keep both chr formats
+  
+  
+  # List to store the placeholders for missing arms
+  missing_arms_list <- list()
+  
+  # Iterate through chromosomes present in y
+  for (i in 1:nrow(y_arm_presence)) {
+    current_chr_num <- y_arm_presence$chr[i]
+    current_chr_std <- y_arm_presence$chr_std_y[i]
+    has_p_coverage <- y_arm_presence$has_p[i]
+    has_q_coverage <- y_arm_presence$has_q[i]
+    
+    # Look up boundaries for this chromosome
+    boundaries <- arm_boundaries[chr_std == current_chr_std]
+    
+    if (nrow(boundaries) == 1) { # Ensure boundaries were found
+      # Check if p arm is missing
+      if (!has_p_coverage && !is.na(boundaries$p_start) && !is.na(boundaries$p_end)) {
+        message(paste("Creating placeholder for missing 'p' arm on chromosome", current_chr_num))
+        p_placeholder <- data.table(
+          chr = current_chr_num,
+          startpos = boundaries$p_start,
+          endpos = boundaries$p_end,
+          seglength = boundaries$p_end - boundaries$p_start,
+          arm = "p"
+          # y_id = NA, # Or assign a unique negative ID? Let's omit for now.
+          # chr_std_y = current_chr_std # Keep consistent? Added below if needed.
+        )
+        missing_arms_list[[length(missing_arms_list) + 1]] <- p_placeholder
+      }
+      
+      # Check if q arm is missing
+      if (!has_q_coverage && !is.na(boundaries$q_start) && !is.na(boundaries$q_end)) {
+        message(paste("Creating placeholder for missing 'q' arm on chromosome", current_chr_num))
+        q_placeholder <- data.table(
+          chr = current_chr_num,
+          startpos = boundaries$q_start,
+          endpos = boundaries$q_end,
+          seglength = boundaries$q_end - boundaries$q_start,
+          arm = "q"
+        )
+        missing_arms_list[[length(missing_arms_list) + 1]] <- q_placeholder
+      }
+    } else {
+      warning(paste("Could not find arm boundary information in 'x' for chromosome:", current_chr_std))
+    }
+  }
+  
+  # D. Append Placeholders
+  # Remove temporary columns from y before binding
+  y[, c("y_id", "chr_std_y") := NULL]
+  
+  if (length(missing_arms_list) > 0) {
+    # Combine all placeholders
+    missing_arms_dt <- rbindlist(missing_arms_list)
+    
+    # Ensure column order consistency (optional, but good practice)
+    # setcolorder(missing_arms_dt, names(y)) # This might fail if columns aren't exact matches yet
+    
+    # Append to the original y data
+    # Use fill=TRUE to handle potential missing columns (like y_id if it wasn't removed)
+    y_final <- rbindlist(list(y, missing_arms_dt), use.names = TRUE, fill = TRUE)
+    message(paste("Appended", nrow(missing_arms_dt), "placeholder arm segments."))
+  } else {
+    y_final <- y # No placeholders to add
+    message("No missing arms found to append.")
+  }
+  
+  # Clean up potential NA values from fill=TRUE if necessary (e.g., y_id column if kept)
+  # y_final[is.na(y_id), y_id := -seq_len(.N)] # Example: assign negative IDs
+  
+  # --- Display Final Result ---
+  print(y_final)
+  
+  # Display Arm Boundaries for reference
+  print("Calculated Arm Boundaries:")
+  print(arm_boundaries)
+  return(y_final)
+}
+
+
+alignCNmatrices <- function(cn_karyo, scRNAseq){
+  
+  a=colnames(cn_karyo)
+  b=scRNAseq$anno
+  B = scRNAseq$cn
+  A = cn_karyo
+
+  cat("Original Matrix A dimensions:", dim(A), "\n")
+  cat("Original Matrix A colnames:", colnames(A), "\n\n")
+  cat("Original Matrix B dimensions:", dim(B), "\n")
+  cat("Original Matrix B colnames:", colnames(B), "\n\n")
+  
+  
+  # --- 1. & 2. Define Target Columns and Create Mapping ---
+  map_list <- list()
+  special_cols <- c("Cell", "MARKER")
+  
+  # Iterate through each high-resolution segment in b
+  for (i in 1:nrow(b)) {
+    b_segment_name <- rownames(b)[i]
+    chr_num <- b$chr[i]
+    arm_type <- b$arm[i]
+    
+    # Handle potential X chromosome naming
+    chr_char <- as.character(chr_num)
+    # if (chr_num == 23) chr_char <- "X" # Adapt if needed
+    
+    original_a_col_p <- paste0(chr_char, "p")
+    original_a_col_q <- paste0(chr_char, "q")
+    
+    if (arm_type == "p") {
+      map_list[[length(map_list) + 1]] <- data.table(
+        target_col = b_segment_name,
+        original_a_col = original_a_col_p,
+        original_b_col = b_segment_name
+      )
+    } else if (arm_type == "q") {
+      map_list[[length(map_list) + 1]] <- data.table(
+        target_col = b_segment_name,
+        original_a_col = original_a_col_q,
+        original_b_col = b_segment_name
+      )
+    } else if (arm_type == "whole") {
+      # Check if corresponding p/q arms exist in the low-res annotation 'a'
+      p_exists_in_a <- original_a_col_p %in% a
+      q_exists_in_a <- original_a_col_q %in% a
+      
+      # If p arm exists, create a target column for it
+      if (p_exists_in_a) {
+        target_p_name <- paste0(b_segment_name, "_p")
+        map_list[[length(map_list) + 1]] <- data.table(
+          target_col = target_p_name,
+          original_a_col = original_a_col_p,
+          original_b_col = b_segment_name # Maps back to the 'whole' column in B
+        )
+      }
+      # If q arm exists, create a target column for it
+      if (q_exists_in_a) {
+        target_q_name <- paste0(b_segment_name, "_q")
+        map_list[[length(map_list) + 1]] <- data.table(
+          target_col = target_q_name,
+          original_a_col = original_a_col_q,
+          original_b_col = b_segment_name # Maps back to the 'whole' column in B
+        )
+      }
+      # If neither exists (unlikely for standard chromosomes), skip? Or log warning.
+      if (!p_exists_in_a && !q_exists_in_a) {
+        warning(paste("Segment", b_segment_name, "is 'whole', but neither",
+                      original_a_col_p, "nor", original_a_col_q, "found in 'a'. Skipping."))
+      }
+    }
+  }
+  
+  # Combine map entries
+  mapping_table <- rbindlist(map_list)
+  
+  # Define the final target column names
+  target_colnames <- unique(c(special_cols, mapping_table$target_col))
+  
+  cat("--- Mapping Created ---\n")
+  print(mapping_table)
+  cat("\nTarget Columns (", length(target_colnames), "):", target_colnames, "\n\n")
+  
+  
+  # --- 3. Reconstruct Matrix A ---
+  cat("--- Reconstructing Matrix A ---\n")
+  # Create the new matrix structure with NAs
+  A_new <- matrix(NA_real_,
+                  nrow = nrow(A),
+                  ncol = length(target_colnames),
+                  dimnames = list(rownames(A), target_colnames))
+  
+  # Copy special columns directly
+  for (scol in special_cols) {
+    if (scol %in% colnames(A) && scol %in% colnames(A_new)) {
+      A_new[, scol] <- A[, scol]
+      cat("Copied special column:", scol, "\n")
+    }
+  }
+  
+  # Populate other columns using the mapping table
+  # Iterate through unique low-resolution columns needed from A
+  a_cols_needed <- unique(mapping_table$original_a_col)
+  for (a_col in a_cols_needed) {
+    # Find all target columns that map to this original A column
+    target_cols_for_a <- mapping_table[original_a_col == a_col, target_col]
+    
+    # Check if the column exists in the original A
+    if (a_col %in% colnames(A)) {
+      cat("Mapping data from A[,'", a_col, "'] to A_new columns: ", paste(target_cols_for_a, collapse=", "), "\n", sep="")
+      # Copy data from the original A column to all corresponding new target columns
+      # Matrix subsetting handles the duplication automatically if target_cols_for_a has multiple elements
+      A_new[, target_cols_for_a] <- A[, a_col]
+    } else {
+      warning(paste("Original column", a_col, "needed for mapping not found in Matrix A. Target columns will be NA."))
+      # Columns in A_new remain NA by default
+    }
+  }
+  cat("Finished Reconstructing Matrix A.\n\n")
+  
+  
+  # --- 4. Reconstruct Matrix B ---
+  cat("--- Reconstructing Matrix B ---\n")
+  # Create the new matrix structure with NAs
+  B_new <- matrix(NA_real_,
+                  nrow = nrow(B),
+                  ncol = length(target_colnames),
+                  dimnames = list(rownames(B), target_colnames))
+  
+  # Handle special columns (initialize, e.g., with 0 or NA)
+  # Here initializing with NA, adjust if 0 or another value is more appropriate
+  for (scol in special_cols) {
+    if (scol %in% colnames(B_new)) {
+      B_new[, scol] <- NA # Or 0.0, depending on expected data type/meaning
+      cat("Initialized special column:", scol, "with NA\n")
+    }
+  }
+  
+  
+  # Populate other columns using the mapping table
+  # Iterate through unique high-resolution columns needed from B
+  b_cols_needed <- unique(mapping_table$original_b_col)
+  for (b_col in b_cols_needed) {
+    # Find all target columns that map to this original B column
+    # (this handles the 1-to-many mapping for split 'whole' columns)
+    target_cols_for_b <- mapping_table[original_b_col == b_col, target_col]
+    
+    # Check if the column exists in the original B
+    if (b_col %in% colnames(B)) {
+      cat("Mapping data from B[,'", b_col, "'] to B_new columns: ", paste(target_cols_for_b, collapse=", "), "\n", sep="")
+      # Copy data from the original B column to all corresponding new target columns
+      # Matrix subsetting handles the duplication automatically
+      B_new[, target_cols_for_b] <- B[, b_col]
+    } else {
+      warning(paste("Original column", b_col, "needed for mapping not found in Matrix B. Target columns will be NA."))
+      # Columns in B_new remain NA
+    }
+  }
+  cat("Finished Reconstructing Matrix B.\n\n")
+  
+  A_new = apply(A_new,2,as.numeric)
+  A_new = A_new[,!colnames(A_new) %in% c("Cell", "MARKER" )]
+  B_new = B_new[,!colnames(B_new) %in% c("Cell", "MARKER" )]
+  rownames(A_new) = rownames(A)
+  rownames(B_new) = rownames(B)
+  
+  # --- Final Check ---
+  cat("Final Matrix A_new dimensions:", dim(A_new), "\n")
+  # print(head(A_new[, 1:min(10, ncol(A_new))])) # Print head of first few columns
+  cat("Final Matrix B_new dimensions:", dim(B_new), "\n")
+  # print(head(B_new[, 1:min(10, ncol(B_new))])) # Print head of first few columns
+  
+  # Verify columns are identical
+  all_cols_match <- identical(colnames(A_new), colnames(B_new))
+  cat("\nColumn names of A_new and B_new are identical:", all_cols_match, "\n")
+  
+  # Optional: Assign the new matrices back to A and B
+  # A <- A_new
+  # B <- B_new
+  # rm(A_new, B_new, mapping_table) # Clean up intermediate objects
+  return(list(cn_karyo=A_new, cn_scRNAseq=B_new))
 }
